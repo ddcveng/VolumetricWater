b869f7bf6e45888b3803906f8b8efc3e0ec8b19b
Eduard Hopfer <edo15115@gmail.com>
2021-06-13 21:05:59 +0200

    finished implementation

--- 02-3dScene/main.cpp
+++ 02-3dScene/main.cpp
@@ -85,6 +85,7 @@ GLuint waterNormal = 0;
 GLuint waterDuDv = 0;
 GLuint testTex = 0;
 GLuint checkerTex = 0;
+GLuint terracotaTex = 0;
 
 // Texture sampler to use
 Sampler activeSampler = Sampler::Nearest;
@@ -96,10 +97,13 @@ FrameBuffer refraction = {0};
 // Control variables
 constexpr float water_height = 0.8f;
 constexpr float ground_height = 1.0f;
-constexpr float infinity = 100.0f;
-constexpr float pool_width = 3.0f;
-constexpr float pool_length = 2.0f;
-constexpr float pool_depth = 1.0f; 
+constexpr float infinity = 1000.0f;
+constexpr float pool_width = 4.0f;
+constexpr float pool_length = 8.0f;
+constexpr float pool_depth = 2.0f; // this has to be a whole number for some reason or the pool gets placed wrong
+constexpr float wave_speed = 0.02f;
+float wave_offset = 0.0f;
+glm::vec4 clipping_plane(0.0f, -1.0f, 0.0f, 0.0f);
 
 // Vsync on?
 bool vsync = true;
@@ -187,7 +191,7 @@ void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods
 // Helper method for creating scene geometry
 void createGeometry()
 {
-    //create a gridSize*gridSize grid of quad patches
+    // Prepare meshes
     quad = Geometry::CreateQuadTex();
     pool = Geometry::CreatePoolTex();
     cube = Geometry::CreateCubeTex();
@@ -197,6 +201,7 @@ void createGeometry()
     waterDuDv = Textures::LoadTexture("data/waterDUDV.png", false);
     checkerTex = Textures::CreateCheckerBoardTexture(256, 16);
     testTex = Textures::LoadTexture("data/512-UV-test.png", false);
+    terracotaTex = Textures::LoadTexture("data/Terracotta_Tiles_002_Base_Color.jpg", false);
     
     textures.CreateSamplers();
 }
@@ -282,11 +287,33 @@ void shutDown()
         glDeleteProgram(shaderProgram[i]);
     }
 
-    // Delete the pool mesh
+    // Release meshes
     delete quad;
     quad = nullptr;
     delete pool;
     pool = nullptr;
+    delete cube;
+    cube = nullptr;
+
+    // Release textures
+    if (glIsTexture(checkerTex))
+        glDeleteTextures(1, &checkerTex);
+    if (glIsTexture(terracotaTex))
+        glDeleteTextures(1, &terracotaTex);
+    if (glIsTexture(testTex))
+        glDeleteTextures(1, &testTex);
+    if (glIsTexture(reflection.color))
+        glDeleteTextures(1, &reflection.color);
+    if (glIsTexture(refraction.color))
+        glDeleteTextures(1, &refraction.color);
+    if (glIsTexture(refraction.depth_stencil))
+        glDeleteTextures(1, &refraction.depth_stencil);
+    if (glIsTexture(reflection.depth_stencil))
+        glDeleteTextures(1, &reflection.depth_stencil);
+
+    // Release buffers
+    glDeleteFramebuffers(1, &reflection.handle);
+    glDeleteFramebuffers(1, &refraction.handle);
 
     // Release the window
     glfwDestroyWindow(mainWindow);
@@ -338,7 +365,6 @@ void processInput(float dt)
     camera.SetTransformation(glm::vec3(0.0f, 0.0f, -5.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 }
 
-// should be fine but !!!
 FrameBuffer createFramebuffer(int width, int height, bool depth_texture)
 {
     // Bind the default framebuffer
@@ -368,6 +394,7 @@ FrameBuffer createFramebuffer(int width, int height, bool depth_texture)
 
     if (depth_texture)
     {
+        // we intend to sample the depth buffer -> it has to be a texture
         glGenTextures(1, &depthStencil);
         glBindTexture(GL_TEXTURE_2D, depthStencil);
         glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, width, height, 0, GL_DEPTH_COMPONENT, GL_UNSIGNED_BYTE, 0);
@@ -419,7 +446,7 @@ void setupFramebuffer(GLuint fbo, bool useStencil = false)
     }
 }
 
-void renderGround(const Camera& cam, const glm::vec4& clipping_plane)
+void renderGround(const Camera& cam)
 {
     glUseProgram(shaderProgram[ShaderProgram::Default]);
 
@@ -446,12 +473,14 @@ void renderGround(const Camera& cam, const glm::vec4& clipping_plane)
     glBindVertexArray(0);
 }
 
-void renderPool(const Camera& cam, const glm::vec4& clipping_plane)
+void renderPool(const Camera& cam)
 {
     glUseProgram(shaderProgram[ShaderProgram::Default]);
     
     glm::mat4 modelToWorld = glm::scale(glm::vec3(pool_width, pool_depth, pool_length));
-    modelToWorld = glm::translate(modelToWorld, glm::vec3(0.0, ground_height - pool_depth/2, 0.0));
+    
+    float offset = pool_depth / 2.0f;
+    modelToWorld = glm::translate(modelToWorld, glm::vec3(0.0, ground_height - offset, 0.0));
 
     //set uniforms
     glUniformMatrix4fv(0, 1, GL_FALSE, glm::value_ptr(modelToWorld));
@@ -473,19 +502,44 @@ void renderPool(const Camera& cam, const glm::vec4& clipping_plane)
     glBindVertexArray(0);
 }
 
-void renderExtras(const Camera& cam, const glm::vec4& clipping_plane)
+void renderExtras(const Camera& cam)
 {
     glUseProgram(shaderProgram[ShaderProgram::Default]);
 
+    glm::mat4 modelToWorld = glm::translate(glm::vec3(0.0f, ground_height + 0.5f, pool_length / 2.0f + 0.5f));
     //set uniforms
-    glUniform4fv(0, 1, glm::value_ptr(clipping_plane));
+    glUniformMatrix4fv(0, 1, GL_FALSE, glm::value_ptr(modelToWorld));
+    glUniformMatrix4fv(1, 1, GL_FALSE, glm::value_ptr(cam.GetWorldToView()));
+    glUniformMatrix4fv(2, 1, GL_FALSE, glm::value_ptr(cam.GetProjection()));
+    glUniform4fv(3, 1, glm::value_ptr(clipping_plane));
 
     //set textures
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, testTex);
+    glBindSampler(0, textures.GetSampler(activeSampler));
 
+    //cube 1
     glBindVertexArray(cube->GetVAO());
     glDrawElements(GL_TRIANGLES, cube->GetIBOSize(), GL_UNSIGNED_INT, reinterpret_cast<void*>(0));
 
-    // draw more cubes
+    modelToWorld = glm::translate(glm::vec3(pool_width / 2.0f + 0.5f + 0.2f, ground_height + 0.5f, 0.2f));
+
+    glUniformMatrix4fv(0, 1, GL_FALSE, glm::value_ptr(modelToWorld));
+
+    glBindTexture(GL_TEXTURE_2D, checkerTex);
+
+    //cube 2
+    glDrawElements(GL_TRIANGLES, cube->GetIBOSize(), GL_UNSIGNED_INT, reinterpret_cast<void*>(0));
+
+    modelToWorld = glm::translate(glm::vec3(pool_width / 2.0f + 0.5f, ground_height + 0.5f + 1.0f, 0.2f));
+    modelToWorld = glm::rotate(modelToWorld, glm::pi<float>() / 3.0f, glm::vec3(0.0f, 1.0f, 0.0f));
+
+    glUniformMatrix4fv(0, 1, GL_FALSE, glm::value_ptr(modelToWorld));
+    glBindTexture(GL_TEXTURE_2D, terracotaTex);
+
+    //cube 3
+    glDrawElements(GL_TRIANGLES, cube->GetIBOSize(), GL_UNSIGNED_INT, reinterpret_cast<void*>(0));
+
 
     // release resources
     glUseProgram(0);
@@ -499,18 +553,52 @@ void renderWater(const Camera& cam, float dt)
 
     glm::mat4 modelToWorld = glm::scale(glm::vec3(pool_width, 1.0, pool_length));
     modelToWorld = glm::translate(modelToWorld, glm::vec3(0.0f, water_height, 0.0f));
+    const glm::vec3 cameraPos = cam.GetViewToWorld()[3];
+    glm::vec2 nearFar(nearClipPlane, farClipPlane);
     
+    wave_offset += wave_speed * dt;
+    wave_offset = fmodf(wave_offset, 1.0);
+
+    float tileX, tileY;
+    if (pool_width > pool_length) {
+        tileX = pool_width / pool_length;
+        tileY = 1;
+    } else {
+        tileX = 1;
+        tileY = pool_length / pool_width;
+    }
+    glm::vec2 tiling(tileX, tileY);
+
     // set uniforms
     glUniformMatrix4fv(0, 1, GL_FALSE, glm::value_ptr(modelToWorld));
     glUniformMatrix4fv(1, 1, GL_FALSE, glm::value_ptr(cam.GetWorldToView()));
     glUniformMatrix4fv(2, 1, GL_FALSE, glm::value_ptr(cam.GetProjection()));
-    //glUniform4fv(3, 1, glm::value_ptr(clipping_plane));
-
+    glUniform3fv(3, 1, glm::value_ptr(cameraPos));
+    glUniform1f(4, wave_offset);
+    glUniform2fv(5, 1, glm::value_ptr(nearFar));
+    glUniform2fv(6, 1, glm::value_ptr(tiling));
+    
     // set textures
     glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_2D, testTex);
+    glBindTexture(GL_TEXTURE_2D, refraction.color);
     glBindSampler(0, textures.GetSampler(activeSampler));
     
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_2D, reflection.color);
+    glBindSampler(1, textures.GetSampler(activeSampler));
+
+    glActiveTexture(GL_TEXTURE2);
+    glBindTexture(GL_TEXTURE_2D, waterNormal);
+    glBindSampler(2, textures.GetSampler(activeSampler));
+
+    glActiveTexture(GL_TEXTURE3);
+    glBindTexture(GL_TEXTURE_2D, waterDuDv);
+    glBindSampler(3, textures.GetSampler(activeSampler));
+
+    glActiveTexture(GL_TEXTURE4);
+    glBindTexture(GL_TEXTURE_2D, refraction.depth_stencil);
+    glBindSampler(4, textures.GetSampler(activeSampler));
+
     // draw
     glBindVertexArray(quad->GetVAO());
     glDrawElements(GL_TRIANGLES, quad->GetIBOSize(), GL_UNSIGNED_INT, reinterpret_cast<void*>(0));
@@ -525,10 +613,10 @@ void renderScene(float dt)
     // first render everything under the water for refractions
     setupFramebuffer(refraction.handle, false);
 
-    glm::vec4 clipping_plane(0.0, -1.0, 0.0, water_height);
+    clipping_plane.w = water_height;
     // draw ...
-    renderPool(camera, clipping_plane);
-    renderGround(camera, clipping_plane); // TODO: stencil buffer needed here too for arbitrary ground planes
+    renderPool(camera);
+    //renderGround(camera, clipping_plane); // TODO: stencil buffer needed here too for arbitrary ground planes
 
     // now render everything above the water for reflections
     setupFramebuffer(reflection.handle, false);
@@ -557,9 +645,9 @@ void renderScene(float dt)
     clipping_plane.y *= -1;
 
     // draw ...
-    renderGround(cam, clipping_plane);
-    renderPool(cam, clipping_plane);
-    //renderExtras(cam, clipping_plane);
+    //renderGround(cam, clipping_plane);
+    renderPool(cam);
+    renderExtras(cam);
 
     // now draw everything in the scene
     // plus water with reflection and refraction
@@ -579,19 +667,20 @@ void renderScene(float dt)
     glStencilMask(0xFF);
 
     // populate the stencil buffer
-    renderPool(camera, clipping_plane);
+    renderPool(camera);
 
     glStencilFunc(GL_NOTEQUAL, 1, 0xFF);
     glStencilMask(0x00);
     glDisable(GL_DEPTH_TEST);
-
-    renderGround(camera, clipping_plane);
-
-    glDisable(GL_STENCIL_TEST);
-    glEnable(GL_DEPTH_TEST);
     glEnable(GL_CULL_FACE);
     glCullFace(GL_BACK);
 
+    renderGround(camera);
+
+    glDisable(GL_STENCIL_TEST);
+    glEnable(GL_DEPTH_TEST);
+
+    renderExtras(camera);
     renderWater(camera, dt);
 
     glBindVertexArray(0);
--- 02-3dScene/shaders.h
+++ 02-3dScene/shaders.h
@@ -65,16 +65,28 @@ R"(
 layout (location = 0) uniform mat4 modelToWorld;
 layout (location = 1) uniform mat4 worldToView;
 layout (location = 2) uniform mat4 projection;
+layout (location = 3) uniform vec3 cameraPosWorld;
+layout (location = 4) uniform float movement;
+layout (location = 5) uniform vec2 nearFar;
+layout (location = 6) uniform vec2 tiling;
 
 layout (location = 0) in vec3 position;
 layout (location = 1) in vec2 texCoords;
 
 out vec2 vTexCoord;
+out vec4 posClipSpace;
+out vec3 pixelToCam;
 
 void main()
 {
-    vTexCoord = texCoords;
-    gl_Position = projection * worldToView * modelToWorld * vec4(position, 1.0);
+    // if the pool is now a square, scale one direction appropriately to tile the texture
+    vTexCoord = vec2(texCoords.x * tiling.x, texCoords.y * tiling.y);
+
+    vec4 positionWorld = modelToWorld * vec4(position, 1.0);
+    pixelToCam = cameraPosWorld - positionWorld.xyz;
+    posClipSpace = projection * worldToView * positionWorld;
+
+    gl_Position = posClipSpace;
 }
 )"
 };
@@ -114,16 +126,104 @@ void main()
 R"(
 #version 460 core
 
-layout (binding = 0) uniform sampler2D diffuse;
+layout (location = 4) uniform float movement;
+layout (location = 5) uniform vec2 nearFar;
+
+layout (binding = 0) uniform sampler2D refraction;
+layout (binding = 1) uniform sampler2D reflection;
+layout (binding = 2) uniform sampler2D normalMap;
+layout (binding = 3) uniform sampler2D dudvMap;
+layout (binding = 4) uniform sampler2D depthBuffer;
 
 in vec2 vTexCoord;
+in vec4 posClipSpace;
+in vec3 pixelToCam;
+
+const float distortionStrenght = 0.01;
+const float offsetFactor = 0.1;
+const float depthScale = 0.91; // how soon will the deep water color appear, it's pretty sensitive
+const float fogDensity = 1.1;
+
+const float n1 = 1.0;  // air
+const float n2 = 1.33; // water
+
+
+const int fresnelPower = 4;
+const float r0 = (n1 - n2) / (n1 + n2);
+// in case of air -> water reflection/refraction this is very small
+// and can very well be approximated as 0 with similar effect
+const float R0 = r0 * r0;
 
 layout (location = 0) out vec4 color;
 
+// returns the linearized depth buffer value in view space
+float linearize_depth(float depthVal)
+{
+    return 2 * nearFar.x * nearFar.y / (nearFar.y + nearFar.x - (nearFar.y - nearFar.x) * (2 * depthVal - 1));
+}
+
 void main()
 {
-  vec3 texSample = texture(diffuse, vTexCoord).rgb;
-  color = vec4(texSample, 1.0f);
+  // convert fragment position from clip space to normalized device space
+  vec2 ndsCoord = (posClipSpace.xy / posClipSpace.w) / 2.0 + 0.5;
+  vec2 refractCoord = ndsCoord;
+  vec2 reflectCoord = vec2(ndsCoord.x, -ndsCoord.y);
+
+  // sample the dudvMap with respect to movement to get distorted coords
+  // these are used to get the actual distortion as well as the normal at that position
+  vec2 offsetCoord = texture(dudvMap, vec2(vTexCoord.x + movement, vTexCoord.y)).xy * offsetFactor;
+  offsetCoord = vTexCoord + vec2(offsetCoord.x, offsetCoord.y + movement);
+
+  // use the distorted coords to get the actual distortion
+  vec2 offset = (texture(dudvMap, offsetCoord).xy * 2.0 - 1.0) * distortionStrenght;
+
+  // offset the nds coords
+  // clamp the values to avoid artifacts at the edges of the pool
+  refractCoord += offset;
+  refractCoord = clamp(refractCoord, 0.001, 0.999);
+
+  reflectCoord += offset;
+  reflectCoord.x = clamp(reflectCoord.x, 0.001, 0.999);
+  reflectCoord.y = clamp(reflectCoord.y, -0.999, -0.001);
+
+  vec4 refractCol = vec4(texture(refraction, refractCoord).rgb, 1.0);
+  vec4 reflectCol = vec4(texture(reflection, reflectCoord).rgb, 1.0);
+
+  // sample the depth buffer to get distance from surface of water to floor
+  // and use it to create fogginess in deeper parts
+  float distToFloor = texture(depthBuffer, ndsCoord).x;
+  distToFloor = linearize_depth(distToFloor);
+
+  float distToWater = gl_FragCoord.z;
+  distToWater = linearize_depth(distToWater);
+
+  float sliceDepth = distToFloor - distToWater;
+  sliceDepth = clamp(sliceDepth, 0.01, 0.99);
+  sliceDepth *= depthScale;
+  float fogAmount = 1.0 - exp( -distToWater * fogDensity);
+  
+  vec4 deepBlue = vec4(0.0, 0.0, 0.247, 1.0);
+  vec4 fog1 = mix(refractCol, deepBlue, fogAmount); // fog based on distance from the pool
+  vec4 fog2 = mix(refractCol, deepBlue, sliceDepth); // fog based on pool depth
+
+  refractCol = mix(fog1, fog2, 0.5);
+
+  vec3 normalRaw = texture(normalMap, offsetCoord).rgb;
+  
+  // convert rgb normal data to the actual vector it represents
+  // also scale it a bit in the y direction so the water is not too bumpy
+  vec3 normal = vec3(normalRaw.r * 2.0 - 1.0, 2.0 * normalRaw.b, normalRaw.g * 2.0 - 1.0);
+
+  normal = normalize(normal);
+  vec3 pixelToCamN = normalize(pixelToCam);
+
+  float cosT = dot(pixelToCamN, normal);
+
+  // Schlick's approximation
+  float R = R0 + (1 - R0) * pow(1 - cosT, fresnelPower);
+  R = clamp(R, 0.05, 0.95);
+
+  color = mix(refractCol, reflectCol, R);
 }
 )"
 };
